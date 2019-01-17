aa99ddb643c36c62098156c0ac7ee371a6703ad2
urashima9616 <urashima9616@gmail.com>
2019-01-16 17:32:08 -0800

    cleanup

--- NetworkReconstruction/utility/gradient_num_calc.m
+++
@@ -1,6 +0,0 @@
-function gradient = gradient_num_calc(P_est, P_k_est, m, k,  G_sample, psi)
-
-
-
-
-end
--- NetworkReconstruction/utility/test_MFNG_gen.m
+++
@@ -1,6 +0,0 @@
-%Test MFNG_gen
-N = 1024;
-avg_d = 4;
-G = MFNG_gen(N, avg_d, 0);
-D = degree(G);
-plot(G);
